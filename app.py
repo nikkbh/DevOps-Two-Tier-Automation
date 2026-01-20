6ccb7083846eb596c3711002ae3fc4e9080b7cad
nikkbh <nikhilbhutani111@yahoo.com>
2026-01-20 21:10:00 +0530

    feat: flask api

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "ok",
+        "health": "healthy",
+        "timestamp": datetime.now().isoformat()
+    }), 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    
