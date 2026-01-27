e9209846c783e9193fa5408b6ed20eef18a0032d
nikkbh <nikhilbhutani111@yahoo.com>
2026-01-27 21:20:32 +0530

    chore: update terraform version

--- infra/terraform/networking/main.tf
+++ infra/terraform/networking/main.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.75.0"
+      version = "=4.1.0"
     }
   }
-  required_version = "~>3.0"
 }
 
 provider "azurerm" {
