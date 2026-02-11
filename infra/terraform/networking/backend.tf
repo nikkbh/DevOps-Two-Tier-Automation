160c169a6e78c309ad4bb722e36f03da98f43fbf
nikkbh <nikhilbhutani111@yahoo.com>
2026-02-11 23:37:44 +0530

    feat: add cicd and update docker-compose.yaml

--- infra/terraform/networking/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    key                  = "networking/terraform.tfstate"
-    resource_group_name  = "rg-tfstate"
-    storage_account_name = "tfstatestorage1769447065"
-    container_name       = "tfstate"
-    use_oidc             = true
-  }
-}
